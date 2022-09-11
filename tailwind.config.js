b72298920318333f6e257fba985d53caa648a9e4
Kedar <kedarmakode1598@gmail.com>
2022-09-11 21:08:57 +0530

    Started Project

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {screens:{
+    sm:'480px',
+    md:'768px',
+    lg:'1020px',
+    xl:'1440px'
+  },
+    extend: {},
+  },
+  plugins: [],
+}
